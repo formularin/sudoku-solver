0f6c10c1da1816fa06b213f524a5607327c6f214
lol-cubes <arinmkhare@gmail.com>
2020-03-23 13:56:20 -0400

    setup file structure

---
+++ include/board.hpp
@@ -0,0 +1,12 @@
+/*=======================================
+ board.hpp:                     lol-cubes
+ last modified:               Mon, Mar 23
+ 
+ This file contains the `board` class,
+ which represents a sudoku board.
+========================================*/
+
+
+class Board {
+    
+}
---
+++ src/solve.cpp
@@ -0,0 +1,8 @@
+/*=======================================
+ solve.cpp:                     lol-cubes
+ last modified:               Mon, Mar 23
+ 
+ This file contains an implementation of
+ the backtracking algorithm to solve a
+ sudoku puzzle.
+========================================*/
